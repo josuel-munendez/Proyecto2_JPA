9fa74cf24081222b0708773e7537d727e5cc748e
Jose Manuel Muñoz Fernandez <fernandezjosemanuel524@gmail.com>
2026-09-21 17:11:11 -0500

    fix: remove unused MySQL/PostgreSQL testcontainers from mongoDB branch, add lock files to gitignore

--- servicio/src/test/java/com/example/servicio/TestcontainersConfiguration.java
+++ servicio/src/test/java/com/example/servicio/TestcontainersConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.mongodb.MongoDBContainer;
-import org.testcontainers.mysql.MySQLContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -15,18 +13,6 @@ class TestcontainersConfiguration {
 	@ServiceConnection
 	MongoDBContainer mongoDbContainer() {
 		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
-	MySQLContainer mysqlContainer() {
-		return new MySQLContainer(DockerImageName.parse("mysql:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
 	}
 
 }
